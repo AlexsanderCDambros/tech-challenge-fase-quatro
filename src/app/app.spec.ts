a34a1cc6b1b99c261121096b065f16b7dade0b3e
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2026-01-02 14:27:52 -0300

    test: ajuste nos testes unitarios e adicao do coverage

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,22 +2,19 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let app: App;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(App);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tech-challenge-fase-quatro');
   });
 });
