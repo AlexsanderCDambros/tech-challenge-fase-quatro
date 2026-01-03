24c7481f8df698cb72d7c426f991ddd86e7e0c6b
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2026-01-03 17:01:59 -0300

    chore: menu e logout funcionando

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { ContextoStore } from '../../shared/stores/contexto-store';
 import { AsyncPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    AsyncPipe
+    AsyncPipe,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
@@ -13,6 +20,11 @@ import { AsyncPipe } from '@angular/common';
 export class Header {
 
   constructor(
-    public contextoStore: ContextoStore
+    public contextoStore: ContextoStore,
+    public router: Router
   ) {}
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
